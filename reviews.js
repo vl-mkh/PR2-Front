b2b12262ca982904419992585aa071e8a0545226
vladm <vladmelekh@gmail.com>
2017-10-10 15:29:46 +0300

    Swiper Container

---
+++ reviews.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        centeredSlides: true,
+        paginationClickable: true,
+        spaceBetween: 30
+      });
+
+  })
